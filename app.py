385bcb16acdbdfdb603a2a5af3b3e63186b9eda4
yrq110 <yrq1025@gmail.com>
2016-11-04 00:49:56 +0800

    add exception handle

--- app.py
+++ app.py
@@ -21,24 +21,32 @@ def index():
 def query():
     if request.method == 'GET':
 
+        error = 0
         # read engineID
-        # f = file('data/engine.json')
-        # s = json.load(f)
-        # no = 2
-        # name = 'engine_' + str(no+1)
-        # cx = s['engine'][no][name]['cx']
+        f = file('data/engine.json')
+        s = json.load(f)
+        no = 4
+        name = 'engine_' + str(no+1)
+        cx = s['engine'][no][name]['cx']
 
         # combine search string
         q = request.args.get('q')
 
-        cx = "009989704776709960942:xgqpl9qptva"
-        print type(q)
+        # cx = "009989704776709960942:xgqpl9qptva"
+        # print type(q)
         url = "https://www.googleapis.com/customsearch/v1"
         query_string = {"key":key,"cx":cx,"num":"10","q":q}
         # print "quertstring is" + query_string
         response = requests.request("GET", url, params=query_string)
         json_data = json.loads(response.text)
 
+        try :
+            json_data['items']
+        except:
+            # print json_data['error']
+            error = 1
+            error_msg = 'error_code:' +str(json_data['error']['code'])
+            return render_template('index.html',error=error,error_msg=error_msg)
         # current_page = json_data['queries']['request'][0]['startIndex']/10
         # next_page = current_page+1
         # engine_name = json_data['context']['title']
@@ -54,13 +62,13 @@ def query():
             result.append(item['snippet'])
             results.append(result)
             result =[]
-        print results
+        # print results
             # print ' title:' + item['title']
         # items = json_data['item']
         # print title, link
         # return toJson(response.text)
         # return toJson(items)
-        return render_template('index.html',results=results)
+        return render_template('index.html',results=results,error=error)
 
 
 @app.errorhandler(404)
